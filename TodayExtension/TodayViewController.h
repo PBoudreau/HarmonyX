785036db9124f80d6bff2231dd4c3ffe7c9855f5
Philippe Boudreau <boudreau.philippe@fasterre.com>
2014-11-05 11:34:41 -0500

    Added Today Extension - missing access to pods

---
+++ TodayExtension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayExtension
+//
+//  Created by Philippe Boudreau on 2014-11-05.
+//  Copyright (c) 2014 Fasterre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ TodayExtension/TodayViewController.m
@@ -0,0 +1,60 @@
+//
+//  TodayViewController.m
+//  TodayExtension
+//
+//  Created by Philippe Boudreau on 2014-11-05.
+//  Copyright (c) 2014 Fasterre. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+#import "FSCHarmonyController.h"
+
+static NSString * const USERNAME = @"boudreau.philippe@gmail.com";
+static NSString * const PASSWORD = @"J9TwoaQGDVCOTz6{V_qK";
+static NSString * const HARMONY_IP = @"10.0.1.4";
+static NSUInteger HARMONY_PORT = 5222;
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (IBAction) chromecastButtonTapped: (id) sender
+{
+    [[FSCHarmonyController sharedInstance] clientWithWithUsername: USERNAME
+                                                         password: PASSWORD
+                                                            forIP: HARMONY_IP
+                                                             port: HARMONY_PORT
+                                                       completion: ^(FSCHarmonyClientController *client) {
+                                                           
+                                                           [client startActivity: @"9204546"
+                                                                  withCompletion: ^(id result) {
+                                                                      
+                                                                      NSLog(@"Activity startup result: %@", result);
+                                                                      
+                                                                      [client disconnect];
+                                                                  }];
+                                                       }];
+}
+
+- (IBAction) offButtonTapped: (id) sender
+{
+    [[FSCHarmonyController sharedInstance] clientWithWithUsername: USERNAME
+                                                         password: PASSWORD
+                                                            forIP: HARMONY_IP
+                                                             port: HARMONY_PORT
+                                                       completion: ^(FSCHarmonyClientController *client) {
+                                                           
+                                                           [client turnOffWithCompletion: ^(id result) {
+                                                               
+                                                               NSLog(@"Turn off result: %@", result);
+                                                               
+                                                               [client disconnect];
+                                                           }];
+                                                       }];
+}
+
+@end
